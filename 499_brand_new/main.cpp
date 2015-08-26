f7ca813d370a4b38f96eb8546ff5e01e09bb3f45
chenyangh <hchenyang@hotmail.com>
2015-08-26 09:56:08 -0400

    Initial Commit

---
+++ 499_brand_new/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  499_brand_new
+//
+//  Created by Chenyang Huang on 2015-08-26.
+//  Copyright (c) 2015 Chenyang Huang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
